af55c908195f9e43440ed272e67cae2fcdea168a
Kevin Chen <kevinchen8223@gmail.com>
2013-03-10 01:13:06 -0800

    Created league rails application

---
+++ league/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+League::Application.config.secret_token = '7d11490f869adaba5a82502f66238a96f2a43160e288e08bf4c0d4f26de103a4748d42532f4db93507e81aab9290784155870eb648d16c93f14ef3dfe4b3e96d'
